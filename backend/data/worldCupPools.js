b9b8914158e6d8754ee5480bbcfb531d34cb80a9
MXLIK17 <145512665+MXLIK17@users.noreply.github.com>
2026-07-09 16:06:44 +0000

    Complete World Cup draft pools

--- backend/data/worldCupPools.js
+++ backend/data/worldCupPools.js
@@ -1,33 +1,776 @@
 const worldCupPools = {
+
     GK: {
+
         elite: [
-            { team: "Germany 2014", players: ["Neuer"] },
-            { team: "Italy 2006", players: ["Buffon"] }
+
+            {
+                team: "Brazil 2002",
+                players: [
+                    "Marcos"
+                ]
+            },
+
+            {
+                team: "Italy 2006",
+                players: [
+                    "Gianluigi Buffon"
+                ]
+            },
+
+            {
+                team: "Spain 2010",
+                players: [
+                    "Iker Casillas"
+                ]
+            },
+
+            {
+                team: "Germany 2014",
+                players: [
+                    "Manuel Neuer"
+                ]
+            },
+
+            {
+                team: "Argentina 2022",
+                players: [
+                    "Emiliano Martinez"
+                ]
+            }
+
         ],
+
+
         strong: [
-            { team: "France 2018", players: ["Lloris"] },
-            { team: "Brazil 2002", players: ["Marcos"] }
+
+            {
+                team: "France 1998",
+                players: [
+                    "Fabien Barthez"
+                ]
+            },
+
+            {
+                team: "Netherlands 2010",
+                players: [
+                    "Maarten Stekelenburg"
+                ]
+            },
+
+            {
+                team: "Croatia 2018",
+                players: [
+                    "Danijel Subasic"
+                ]
+            }
+
         ],
+
+
         average: [
-            { team: "USA 2014", players: ["Howard"] },
-            { team: "Japan 2018", players: ["Kawashima"] }
+
+            {
+                team: "South Korea 2002",
+                players: [
+                    "Lee Woon-jae"
+                ]
+            },
+
+            {
+                team: "Costa Rica 2014",
+                players: [
+                    "Keylor Navas"
+                ]
+            },
+
+            {
+                team: "Japan 2022",
+                players: [
+                    "Shuichi Gonda"
+                ]
+            }
+
         ]
+
     },
 
-    CB: {
-        elite: [
-            { team: "Italy 2006", players: ["Cannavaro", "Nesta"] },
-            { team: "Germany 2014", players: ["Hummels", "Boateng"] }
-        ],
-        strong: [
-            { team: "France 2018", players: ["Varane", "Umtiti"] },
-            { team: "Argentina 2014", players: ["Demichelis", "Garay"] }
-        ],
-        average: [
-            { team: "Mexico 2018", players: ["Moreno", "Araujo"] },
-            { team: "Nigeria 2014", players: ["Omeruo", "Yobo"] }
-        ]
-    }
+CB: {
+
+    elite: [
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Fabio Cannavaro",
+                "Alessandro Nesta",
+                "Marco Materazzi"
+            ]
+        },
+
+        {
+            team: "Spain 2010",
+            players: [
+                "Carles Puyol",
+                "Gerard Pique"
+            ]
+        },
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Lucio",
+                "Roque Junior"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Mats Hummels",
+                "Jerome Boateng"
+            ]
+        },
+
+        {
+            team: "France 2018",
+            players: [
+                "Raphael Varane",
+                "Samuel Umtiti"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Argentina 1986",
+            players: [
+                "Daniel Passarella",
+                "Jose Brown"
+            ]
+        },
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "John Heitinga",
+                "Joris Mathijsen"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Dejan Lovren",
+                "Domagoj Vida"
+            ]
+        },
+
+        {
+            team: "Portugal 2006",
+            players: [
+                "Ricardo Carvalho",
+                "Fernando Meira"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Hong Myung-bo",
+                "Kim Tae-young"
+            ]
+        },
+
+        {
+            team: "Ghana 2010",
+            players: [
+                "John Mensah",
+                "Isaac Vorsah"
+            ]
+        },
+
+        {
+            team: "Costa Rica 2014",
+            players: [
+                "Oscar Duarte",
+                "Giancarlo Gonzalez"
+            ]
+        }
+
+    ]
+
+},
+
+LB: {
+
+    elite: [
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Roberto Carlos"
+            ]
+        },
+
+        {
+            team: "Spain 2010",
+            players: [
+                "Joan Capdevila"
+            ]
+        },
+
+        {
+            team: "France 2018",
+            players: [
+                "Lucas Hernandez"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Benedikt Howedes"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Fabio Grosso"
+            ]
+        },
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "Giovanni van Bronckhorst"
+            ]
+        },
+
+        {
+            team: "Argentina 2022",
+            players: [
+                "Marcos Acuna"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Ivan Strinic"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Lee Young-pyo"
+            ]
+        },
+
+        {
+            team: "Costa Rica 2014",
+            players: [
+                "Junior Diaz"
+            ]
+        },
+
+        {
+            team: "Japan 2022",
+            players: [
+                "Yuto Nagatomo"
+            ]
+        }
+
+    ]
+
+},
+
+RB: {
+
+    elite: [
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Cafu"
+            ]
+        },
+
+        {
+            team: "Spain 2010",
+            players: [
+                "Sergio Ramos"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Philipp Lahm"
+            ]
+        },
+
+        {
+            team: "France 2018",
+            players: [
+                "Benjamin Pavard"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Gianluca Zambrotta"
+            ]
+        },
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "Gregory van der Wiel"
+            ]
+        },
+
+        {
+            team: "Portugal 2006",
+            players: [
+                "Miguel"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Sime Vrsaljko"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Song Chong-gug"
+            ]
+        },
+
+        {
+            team: "Ghana 2010",
+            players: [
+                "John Pantsil"
+            ]
+        },
+
+        {
+            team: "Japan 2022",
+            players: [
+                "Hiroki Sakai"
+            ]
+        }
+
+    ]
+
+},
+
+CM: {
+
+    elite: [
+
+        {
+            team: "Spain 2010",
+            players: [
+                "Xavi",
+                "Andres Iniesta",
+                "Sergio Busquets"
+            ]
+        },
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Kaka",
+                "Gilberto Silva",
+                "Kleberson"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Toni Kroos",
+                "Bastian Schweinsteiger",
+                "Sami Khedira"
+            ]
+        },
+
+        {
+            team: "Argentina 2022",
+            players: [
+                "Enzo Fernandez",
+                "Rodrigo De Paul",
+                "Alexis Mac Allister"
+            ]
+        },
+
+        {
+            team: "France 1998",
+            players: [
+                "Zinedine Zidane",
+                "Emmanuel Petit",
+                "Patrick Vieira"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Andrea Pirlo",
+                "Gennaro Gattuso",
+                "Daniele De Rossi"
+            ]
+        },
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "Wesley Sneijder",
+                "Mark van Bommel",
+                "Nigel de Jong"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Luka Modric",
+                "Ivan Rakitic",
+                "Marcelo Brozovic"
+            ]
+        },
+
+        {
+            team: "Portugal 2006",
+            players: [
+                "Deco",
+                "Maniche",
+                "Costinha"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Park Ji-sung",
+                "Lee Young-pyo"
+            ]
+        },
+
+        {
+            team: "Ghana 2010",
+            players: [
+                "Kevin-Prince Boateng",
+                "Sulley Muntari"
+            ]
+        },
+
+        {
+            team: "Japan 2022",
+            players: [
+                "Ao Tanaka",
+                "Wataru Endo"
+            ]
+        },
+
+        {
+            team: "Costa Rica 2014",
+            players: [
+                "Celso Borges",
+                "Yeltsin Tejeda"
+            ]
+        }
+
+    ]
+
+},
+
+FW: {
+
+    elite: [
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Ronaldo",
+                "Rivaldo",
+                "Ronaldinho"
+            ]
+        },
+
+        {
+            team: "Spain 2010",
+            players: [
+                "David Villa",
+                "Pedro",
+                "Andres Iniesta"
+            ]
+        },
+
+        {
+            team: "France 2018",
+            players: [
+                "Kylian Mbappe",
+                "Antoine Griezmann",
+                "Olivier Giroud"
+            ]
+        },
+
+        {
+            team: "Argentina 2022",
+            players: [
+                "Lionel Messi",
+                "Angel Di Maria",
+                "Lautaro Martinez"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Thomas Muller",
+                "Mesut Ozil",
+                "Andre Schurrle"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "Arjen Robben",
+                "Robin van Persie",
+                "Dirk Kuyt"
+            ]
+        },
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Francesco Totti",
+                "Alessandro Del Piero",
+                "Vincenzo Iaquinta"
+            ]
+        },
+
+        {
+            team: "Portugal 2006",
+            players: [
+                "Cristiano Ronaldo",
+                "Luis Figo",
+                "Simao"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Ivan Perisic",
+                "Ante Rebic",
+                "Mario Mandzukic"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Ahn Jung-hwan",
+                "Seol Ki-hyeon"
+            ]
+        },
+
+        {
+            team: "Ghana 2010",
+            players: [
+                "Andre Ayew",
+                "Kevin-Prince Boateng"
+            ]
+        },
+
+        {
+            team: "Japan 2022",
+            players: [
+                "Kaoru Mitoma",
+                "Takefusa Kubo"
+            ]
+        },
+
+        {
+            team: "Costa Rica 2014",
+            players: [
+                "Joel Campbell",
+                "Bryan Ruiz"
+            ]
+        }
+
+    ]
+
+},
+ST: {
+
+    elite: [
+
+        {
+            team: "Brazil 2002",
+            players: [
+                "Ronaldo"
+            ]
+        },
+
+        {
+            team: "Argentina 2022",
+            players: [
+                "Julian Alvarez"
+            ]
+        },
+
+        {
+            team: "France 2018",
+            players: [
+                "Olivier Giroud"
+            ]
+        },
+
+        {
+            team: "Spain 2010",
+            players: [
+                "David Villa"
+            ]
+        },
+
+        {
+            team: "Germany 2014",
+            players: [
+                "Miroslav Klose"
+            ]
+        }
+
+    ],
+
+
+    strong: [
+
+        {
+            team: "Netherlands 2010",
+            players: [
+                "Robin van Persie"
+            ]
+        },
+
+        {
+            team: "Italy 2006",
+            players: [
+                "Luca Toni"
+            ]
+        },
+
+        {
+            team: "Portugal 2006",
+            players: [
+                "Pauleta"
+            ]
+        },
+
+        {
+            team: "Croatia 2018",
+            players: [
+                "Mario Mandzukic"
+            ]
+        }
+
+    ],
+
+
+    average: [
+
+        {
+            team: "South Korea 2002",
+            players: [
+                "Ahn Jung-hwan"
+            ]
+        },
+
+        {
+            team: "Ghana 2010",
+            players: [
+                "Asamoah Gyan"
+            ]
+        },
+
+        {
+            team: "Japan 2022",
+            players: [
+                "Daizen Maeda"
+            ]
+        },
+
+        {
+            team: "Costa Rica 2014",
+            players: [
+                "Joel Campbell"
+            ]
+        }
+
+    ]
+
+}
+
 };
+
 
 module.exports = worldCupPools;
